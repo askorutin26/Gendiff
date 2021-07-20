54dc9531c3fd5c3c2fc19ba71b9ba7872fafbc9b
askor26 <askorutin26@gmail.com>
2021-07-20 19:11:51 +0300

    added .action option that take two files and output differences between them

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
+import genDiff from '../src/showDiff.js';
 
 const program = new Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format[type]', 'output format')
-  .arguments('<file1> <file2>');
+  .arguments('<file1> <file2>')
+  .action((file1, file2) => { console.log(genDiff(file1, file2)); });
 program.parse();
