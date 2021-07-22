ac88035042857c12adf63c9ed736622b56e37cf2
askor26 <askorutin26@gmail.com>
2021-07-22 16:38:25 +0300

    changed logic of parsing

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
-import getFixturePath from './makePath.js';
 
-const parseFile = (fileName) => {
-  const ext = path.extname(fileName);
-  const filePath = getFixturePath(fileName);
-
+const parseFile = (filePath) => {
+  const ext = path.extname(filePath);
+  const fileData = fs.readFileSync(filePath, 'utf-8');
   switch (ext) {
-    case '.yaml' || '.yml':
-      return yaml.load(fs.readFileSync(filePath, 'utf-8'));
-
+    case '.yml':
+    case '.yaml':
+      return (yaml.load(fileData));
     case '.json':
-      return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      return (JSON.parse(fileData));
     default:
-      throw new Error('wrong extension!');
+      throw new Error(console.log(filePath));
   }
 };
 export default parseFile;
