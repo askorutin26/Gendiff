eb4b18a70d6787fc18793955d286b565c0ff1d45
askor26 <askorutin26@gmail.com>
2021-07-22 16:38:35 +0300

    change logic

--- src/showDiff.js
+++ src/showDiff.js
@@ -1,13 +1,13 @@
-import * as fs from 'fs';
 import _ from 'lodash';
 import getFixturePath from './makePath.js';
+import parseFile from './parsers.js';
 
 const genDiff = (file1, file2) => {
-  const file1Path = getFixturePath(file1);
-  const file2Path = getFixturePath(file2);
+  const filePath1 = getFixturePath(file1);
+  const filePath2 = getFixturePath(file2);
 
-  const data1 = JSON.parse(fs.readFileSync(file1Path, 'utf-8'));
-  const data2 = JSON.parse(fs.readFileSync(file2Path, 'utf-8'));
+  const data1 = parseFile(filePath1);
+  const data2 = parseFile(filePath2);
 
   const data1Keys = Object.keys(data1);
   const data2Keys = Object.keys(data2);
