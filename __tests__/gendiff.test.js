9851db754d420fbc2d660c9b5bfae07cd9e3ece2
askor26 <askorutin26@gmail.com>
2021-07-22 16:35:46 +0300

    modified test for gendiff module

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,9 +9,12 @@ const expected = `{
   + timeout: 20
   + verbose: true
 }`;
-const file1 = 'file1.json';
-const file2 = 'file2.json';
+const file1Json = 'file1.json';
+const file2Json = 'file2.json';
+const file1Yml = 'file1.yml';
+const file2Yml = 'file2.yml';
 
 test('genDiffWorks', () => {
-  expect(genDiff(file1, file2)).toBe(expected);
+  expect(genDiff(file1Json, file2Json)).toBe(genDiff(file1Yml, file2Yml));
+  expect(genDiff(file1Json, file2Json)).toBe(expected);
 });
