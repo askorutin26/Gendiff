6cc69252ac754e88ba35e54f2267a7555b44772b
askor26 <askorutin26@gmail.com>
2021-08-25 13:48:28 +0300

    refactored

--- src/showDiff.js
+++ src/showDiff.js
@@ -1,31 +1,18 @@
-import _ from 'lodash';
 import getFixturePath from './makePath.js';
 import parseFile from './parsers.js';
+import buildDiffTree from './buildDiffTree.js';
+import formatData from './formatters/index.js';
 
-const genDiff = (file1, file2) => {
+const genDiff = (file1, file2, formatter) => {
   const filePath1 = getFixturePath(file1);
   const filePath2 = getFixturePath(file2);
 
   const data1 = parseFile(filePath1);
   const data2 = parseFile(filePath2);
 
-  const data1Keys = Object.keys(data1);
-  const data2Keys = Object.keys(data2);
-  const allKeys = _.union(data1Keys, data2Keys).sort();
+  const diffTree = buildDiffTree(data1, data2);
+  const diffTreeAsStr = formatData(diffTree, formatter);
 
-  const reducerLogic = (acc, element) => {
-    if (_.has(data1, element) && _.has(data2, element) && data1[element] === data2[element]) {
-      acc.push(`    ${element}: ${data1[element]}`);
-    }
-    if (_.has(data1, element) && !_.has(data2, element)) { acc.push(`  - ${element}: ${data1[element]}`); }
-    if (_.has(data2, element) && !_.has(data1, element)) { acc.push(`  + ${element}: ${data2[element]}`); }
-    if (_.has(data1, element) && _.has(data2, element) && data1[element] !== data2[element]) {
-      acc.push(`  - ${element}: ${data1[element]}`);
-      acc.push(`  + ${element}: ${data2[element]}`);
-    }
-    return acc;
-  };
-  const result = `{\n${allKeys.reduce(reducerLogic, []).join('\n')}\n}`;
-  return (result);
+  return diffTreeAsStr;
 };
 export default genDiff;
