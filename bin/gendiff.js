73b492053a9922e91f189605ae25f27d92dcba53
askor26 <askorutin26@gmail.com>
2021-08-25 13:47:02 +0300

    refactored

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,11 @@ import { Command } from 'commander';
 import genDiff from '../src/showDiff.js';
 
 const program = new Command();
+
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format[type]', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<file1> <file2>')
-  .action((file1, file2) => { console.log(genDiff(file1, file2)); });
-program.parse();
+  .action((file1, file2) => { console.log(genDiff(file1, file2, `${program.opts().format}`)); });
+program.parse(process.argv);
