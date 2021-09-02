1b265b235f1acdf649c2828f8b13eb6562c7a875
askor26 <askorutin26@gmail.com>
2021-09-02 14:05:07 +0300

    added test for nested yml files and for json format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,8 +3,11 @@ import genDiff from '../src/showDiff.js';
 import parseFile from '../src/parsers.js';
 import getFixturePath from '../src/makePath.js';
 
-const file1NestedPath = 'file1Nested.json';
-const file2NestedPath = 'file2Nested.json';
+const nestedJson1 = 'file1Nested.json';
+const nestedJson2 = 'file2Nested.json';
+
+const nestedYml1 = 'file1Nested.yml';
+const nestedYml2 = 'file2Nested.yml';
 
 const correctStylishPath = getFixturePath('stylishNestedCorrect.txt');
 const rightForNestedStylish = parseFile(correctStylishPath);
@@ -12,9 +15,18 @@ const rightForNestedStylish = parseFile(correctStylishPath);
 const correctPlainPath = getFixturePath('plainNestedCorrect.txt');
 const rightForNestedPlain = parseFile(correctPlainPath);
 
+const correctJsonPath = getFixturePath('jsonCorrect.txt');
+const rigthForJson = parseFile(correctJsonPath);
+
 test('stylishDiffWorks', () => {
-  expect(genDiff(file1NestedPath, file2NestedPath, 'stylish')).toBe(rightForNestedStylish);
+  expect(genDiff(nestedJson1, nestedJson2, 'stylish')).toBe(rightForNestedStylish);
+  expect(genDiff(nestedYml1, nestedYml2, 'stylish')).toBe(rightForNestedStylish);
 });
 test('plainDiffWorks', () => {
-  expect(genDiff(file1NestedPath, file2NestedPath, 'plain')).toBe(rightForNestedPlain);
+  expect(genDiff(nestedJson1, nestedJson2, 'plain')).toBe(rightForNestedPlain);
+  expect(genDiff(nestedYml1, nestedYml2, 'plain')).toBe(rightForNestedPlain);
+});
+test('jsonDiffWorks', () => {
+  expect(genDiff(nestedJson1, nestedJson2, 'json')).toBe(rigthForJson);
+  expect(genDiff(nestedYml1, nestedYml2, 'json')).toBe(rigthForJson);
 });
