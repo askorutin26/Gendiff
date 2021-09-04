a2e1ad46dc1825d9f3cecd6b6ad21d3f57c2313f
askor26 <askorutin26@gmail.com>
2021-09-04 13:45:05 +0300

    deleted string interpolation

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,5 +9,5 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<file1> <file2>')
-  .action((file1, file2) => { console.log(genDiff(file1, file2, `${program.opts().format}`)); });
+  .action((file1, file2) => { console.log(genDiff(file1, file2, program.opts().format)); });
 program.parse(process.argv);
