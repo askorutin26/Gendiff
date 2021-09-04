127a5ceafca19bcfc2e5e8df780f2c18354fa666
askor26 <askorutin26@gmail.com>
2021-09-04 14:46:07 +0300

    deleted makePath module and moved all the logic into index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import path from 'path';
 import * as fs from 'fs';
+import process from 'process';
 import parseFile from './parser.js';
 import buildDiffTree from './buildDiffTree.js';
 import formatData from './formatters/index.js';
-import getPath from './makePath.js';
+
+const getFullPath = (fileName) => {
+  const filePath = path.resolve(process.cwd(), fileName);
+  return filePath;
+};
+const getExtension = (filePath) => path.extname(filePath);
+const getData = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
 const genDiff = (file1, file2, formatter = 'stylish') => {
-  const file1Path = getPath(file1);
-  const file2Path = getPath(file2);
+  const file1Path = getFullPath(file1);
+  const file2Path = getFullPath(file2);
 
-  const file1Ext = path.extname(file1);
-  const file2Ext = path.extname(file2);
+  const file1Ext = getExtension(file1);
+  const file2Ext = getExtension(file2);
 
-  const file1Data = fs.readFileSync(file1Path, 'utf-8');
-  const file2Data = fs.readFileSync(file2Path, 'utf-8');
+  const file1Data = getData(file1Path);
+  const file2Data = getData(file2Path);
 
   const parsedData1 = parseFile(file1Ext, file1Data);
   const parsedData2 = parseFile(file2Ext, file2Data);
--- src/makePath.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import process from 'process';
-
-const getPath = (fileName) => path.resolve(process.cwd(), fileName);
-
-const getFullPath = (filePath) => getPath(filePath);
-
-export default getFullPath;
