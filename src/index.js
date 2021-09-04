24275395e03726686728ae650078d3645482ae73
askor26 <askorutin26@gmail.com>
2021-09-04 14:26:36 +0300

    deleted index.js and renamed: genDiff.js => index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import genDiff from './showDiff.js';
+import path from 'path';
+import * as fs from 'fs';
+import parseFile from './parser.js';
+import buildDiffTree from './buildDiffTree.js';
+import formatData from './formatters/index.js';
+import getPath from './makePath.js';
 
+const genDiff = (file1, file2, formatter = 'stylish') => {
+  const file1Path = getPath(file1);
+  const file2Path = getPath(file2);
+
+  const file1Ext = path.extname(file1);
+  const file2Ext = path.extname(file2);
+
+  const file1Data = fs.readFileSync(file1Path, 'utf-8');
+  const file2Data = fs.readFileSync(file2Path, 'utf-8');
+
+  const parsedData1 = parseFile(file1Ext, file1Data);
+  const parsedData2 = parseFile(file2Ext, file2Data);
+
+  const diffTree = buildDiffTree(parsedData1, parsedData2);
+  const diffOutput = formatData(diffTree, formatter);
+
+  return diffOutput;
+};
 export default genDiff;
