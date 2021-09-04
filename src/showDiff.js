0e595e2110b50e2fdb122132e0075a37224110ac
askor26 <askorutin26@gmail.com>
2021-09-04 14:19:48 +0300

    moved extension and data getter from parsers

--- src/showDiff.js
+++ src/showDiff.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import * as fs from 'fs';
 import parseFile from './parsers.js';
 import buildDiffTree from './buildDiffTree.js';
 import formatData from './formatters/index.js';
@@ -7,8 +9,14 @@ const genDiff = (file1, file2, formatter = 'stylish') => {
   const file1Path = getPath(file1);
   const file2Path = getPath(file2);
 
-  const parsedData1 = parseFile(file1Path);
-  const parsedData2 = parseFile(file2Path);
+  const file1Ext = path.extname(file1);
+  const file2Ext = path.extname(file2);
+
+  const file1Data = fs.readFileSync(file1Path, 'utf-8');
+  const file2Data = fs.readFileSync(file2Path, 'utf-8');
+
+  const parsedData1 = parseFile(file1Ext, file1Data);
+  const parsedData2 = parseFile(file2Ext, file2Data);
 
   const diffTree = buildDiffTree(parsedData1, parsedData2);
   const diffOutput = formatData(diffTree, formatter);
