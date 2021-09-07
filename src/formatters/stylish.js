842db1875a24166ebc5586da4273b73af16817b1
askor26 <askorutin26@gmail.com>
2021-09-07 19:10:38 +0300

    refactored

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,27 +1,27 @@
 import _ from 'lodash';
 
-const repeatIndent = (lvl) => ' '.repeat(lvl);
-const baseIndentCount = 2;
-const objIndentCount = 4;
-const keyIndentCount = 8;
+const repeatIndent = (count, placeholder) => placeholder.repeat(count);
+const indentShift = 2;
+const baseIndentCount = 4;
+const placeholder = ' ';
 
 const toString = (data, indentLvl) => {
   if (!_.isPlainObject(data)) {
     return data;
   }
-  const indentForObj = repeatIndent(indentLvl + objIndentCount);
-  const indentForKey = repeatIndent(indentLvl + keyIndentCount);
-  const result = Object.entries(data).map(([key, value]) => `${indentForKey}${key}: ${toString(value, indentLvl + objIndentCount)}`);
+  const indentForObj = repeatIndent(indentLvl + baseIndentCount, placeholder);
+  const indentForKey = repeatIndent(indentLvl + (indentShift * baseIndentCount), placeholder);
+  const result = Object.entries(data).map(([key, value]) => `${indentForKey}${key}: ${toString(value, indentLvl + baseIndentCount)}`);
   return `{\n${result.join('\n')}\n${indentForObj}}`;
 };
 const toStringHasChildren = (elem, indentLvl, f) => {
   const { key, children } = elem;
-  const indent = repeatIndent(indentLvl + objIndentCount);
-  return `${indent}${key}: {\n${f(children, indentLvl + objIndentCount)}\n${indent}}`;
+  const indent = repeatIndent(indentLvl + baseIndentCount, placeholder);
+  return `${indent}${key}: {\n${f(children, indentLvl + baseIndentCount)}\n${indent}}`;
 };
 const toStringChanged = (elem, indentLvl) => {
   const { key, oldValue, newValue } = elem;
-  const indent = repeatIndent(indentLvl + baseIndentCount);
+  const indent = repeatIndent(indentLvl + indentShift, placeholder);
   return `${indent}- ${key}: ${toString(oldValue, indentLvl)}\n${indent}+ ${key}: ${toString(newValue, indentLvl)}`;
 };
 
@@ -29,14 +29,14 @@ const stylishFormat = (diff) => {
   const iter = (data, lvl = 0) => {
     const result = data.map((elem) => {
       const { key, type, value } = elem;
-      const indent = repeatIndent(lvl);
+      const indent = repeatIndent(lvl + indentShift, placeholder);
       switch (type) {
         case 'added':
-          return `  ${indent}+ ${key}: ${toString(value, lvl)}`;
+          return `${indent}+ ${key}: ${toString(value, lvl)}`;
         case 'deleted':
-          return `  ${indent}- ${key}: ${toString(value, lvl)}`;
+          return `${indent}- ${key}: ${toString(value, lvl)}`;
         case 'unchanged':
-          return `  ${indent}  ${key}: ${toString(value, lvl)}`;
+          return `${indent}  ${key}: ${toString(value, lvl)}`;
         case 'changed':
           return toStringChanged(elem, lvl);
         case 'hasChildren':
