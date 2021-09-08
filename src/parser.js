609e7c063433dc58783747e7e278d9c0d7a330f3
askor26 <askorutin26@gmail.com>
2021-09-08 12:44:55 +0300

    changd extensions to check and deleted excess brackets

--- src/parser.js
+++ src/parser.js
@@ -2,12 +2,12 @@ import yaml from 'js-yaml';
 
 const parseFile = (extension, data) => {
   switch (extension) {
-    case '.yml':
-    case '.yaml':
-      return (yaml.load(data));
-    case '.json':
-      return (JSON.parse(data));
-    case '.txt':
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    case 'json':
+      return JSON.parse(data);
+    case 'txt':
       return data;
     default:
       throw new Error(`wrong extension type: ${extension}`);
